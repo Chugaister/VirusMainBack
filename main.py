c18b8858a2917ba1025ad8a09033ff33f8a05841
Chugaister <dmytro.shvahyliak@gmail.com>
2024-05-11 21:34:09 +0300

    add certs

--- main.py
+++ main.py
@@ -5,5 +5,8 @@ if __name__ == "__main__":
     run(
         app="app:app",
         host=config.HOST,
-        port=config.PORT
+        port=config.PORT,
+        ssl_certfile=config.SSL_CERTFILE_PATH,
+        ssl_keyfile=config.SSL_KEYFILE_PATH,
+        ssl_ca_certs=config.SSL_CA_BUNDLE_FILE_PATH
     )
--- utils/config.py
+++ utils/config.py
@@ -14,11 +14,15 @@ class Config:
         self.HOST = self.get_var("HOST")
         self.PORT = int(self.get_var("PORT"))
         self.SECRET_KEY = self.get_var("SECRET_KEY")
+        self.SECRET_KEY = self.get_var("SECRET_KEY")
+        self.SSL_CERTFILE_PATH = self.get_var("SSL_CERTFILE_PATH", optional=True)
+        self.SSL_KEYFILE_PATH = self.get_var("SSL_KEYFILE_PATH", optional=True)
+        self.SSL_CA_BUNDLE_FILE_PATH = self.get_var("SSL_CA_BUNDLE_FILE_PATH", optional=True)
 
     @staticmethod
-    def get_var(item: str):
+    def get_var(item: str, optional: bool = False):
         var = getenv(item)
-        if not var:
+        if not var and not optional:
             raise EnvVariableNotFound(f"Environment variable {item} not found")
         return var
 
